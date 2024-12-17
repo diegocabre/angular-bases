e2593efeed7cdfc3e08acdba2493510b5d0fb367
Diego Cabré <diegocabre@gmail.com>
2024-12-17 16:59:24 -0300

    Fin de Seccion 4

---
+++ src/app/counter/counter.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CounterComponent } from './components/counter/counter.component';
+
+@NgModule({
+  declarations: [],
+  imports: [CounterComponent],
+  exports: [CounterComponent],
+})
+export class CounterModule {}
